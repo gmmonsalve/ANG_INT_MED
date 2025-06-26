dc1853a85ac2351eaf33be730890d8ea53293bf5
gmmonsalve <gbrielamonsalve@gmail.com>
2025-06-26 07:54:00 -0600

    Add initial config to project

--- courseManager/src/app/app.routes.ts
+++ courseManager/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CourseManagerComponent } from './components/course-manager/course-manager.component';
+import { CourseDetailComponent } from './components/course-detail/course-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: '/cursos', pathMatch: 'full'},
+    {path: 'cursos', component: CoursesComponent, 
+    children: [
+        { path:'detail/:id', component: CourseDetailComponent }
+    ]},
+    {path: 'admin', component: CourseManagerComponent}
+];
