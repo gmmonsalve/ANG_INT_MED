58e8901f1c5641d65f2cd3ea73ee2d47d5ea98e3
gmmonsalve <gbrielamonsalve@gmail.com>
2025-06-26 07:55:35 -0600

    Add courses component

---
+++ courseManager/src/app/components/courses/courses.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CourseService } from '../../services/course.service';
+import { Observable } from 'rxjs';
+import { Course } from '../../interfaces/course.interface';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-courses',
+  imports: [MatCardModule, MatGridListModule, CommonModule],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.css'
+})
+export class CoursesComponent implements OnInit {
+
+  private courseService: CourseService = inject(CourseService)
+  courseList$: Observable<Course[]> | undefined;
+
+  ngOnInit(): void {
+    this.courseList$ = this.courseService.getCourseList()
+  }
+
+}
