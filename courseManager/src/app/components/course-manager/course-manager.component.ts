a0b2328778dd1a95a7594e183f791f10fe259550
gmmonsalve <gbrielamonsalve@gmail.com>
2025-06-26 07:54:58 -0600

    Add course manager component

---
+++ courseManager/src/app/components/course-manager/course-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-manager',
+  imports: [],
+  templateUrl: './course-manager.component.html',
+  styleUrl: './course-manager.component.css'
+})
+export class CourseManagerComponent {
+
+}
