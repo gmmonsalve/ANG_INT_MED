b2b50988d16a8c82688c0a8489a43d9f0c561684
gmmonsalve <gbrielamonsalve@gmail.com>
2025-06-26 07:55:57 -0600

    Add course service

---
+++ courseManager/src/app/services/course.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, inject } from '@angular/core';
+import { Course } from '../interfaces/course.interface';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { env } from '../../env';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+private api = inject(HttpClient)
+
+  getCourseList(): Observable<Course[]>{
+    return this.api.get(`${env.apiEndpoint}/course`) as Observable<Course[]>
+  }
+
+  getDetailCourse(id: string): Observable<Course>{
+    return this.api.get(`${env.apiEndpoint}/course/${id}`) as Observable<Course>
+  }
+
+  createCourse(course: Course): Observable<any>{
+    return this.api.post(`${env.apiEndpoint}/course`, course)
+  }
+
+  updateCourse(course: Course): Observable<any>{
+    return this.api.put(`${env.apiEndpoint}/course`, course)
+  }
+
+  deleteCourse(id: string): Observable<any>{
+    return this.api.delete(`${env.apiEndpoint}/course/${id}`)
+  }
+}
