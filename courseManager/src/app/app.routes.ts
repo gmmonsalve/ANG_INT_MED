a3b433ea1ea01c7c6408d6b97f3cbd83b0ac85dd
gmmonsalve <gbrielamonsalve@gmail.com>
2025-06-27 17:45:12 -0500

    refactor: simplify routes by removing unused CourseDetailComponent

--- courseManager/src/app/app.routes.ts
+++ courseManager/src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './components/courses/courses.component';
 import { CourseManagerComponent } from './components/course-manager/course-manager.component';
-import { CourseDetailComponent } from './components/course-detail/course-detail.component';
 
 export const routes: Routes = [
-    {path:'', redirectTo: '/cursos', pathMatch: 'full'},
-    {path: 'cursos', component: CoursesComponent, 
-    children: [
-        { path:'detail/:id', component: CourseDetailComponent }
-    ]},
-    {path: 'admin', component: CourseManagerComponent}
+    { path:'', redirectTo: '/cursos', pathMatch: 'full' },
+    { path: 'cursos', component: CoursesComponent },
+    { path: 'admin', component: CourseManagerComponent }
 ];
