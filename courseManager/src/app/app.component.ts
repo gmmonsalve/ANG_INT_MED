4120e836c940cc2172fbd2f279e380e07ac9b582
gmmonsalve <gbrielamonsalve@gmail.com>
2025-06-27 17:44:53 -0500

    feat: implement admin view toggle and enhance toolbar layout

--- courseManager/src/app/app.component.ts
+++ courseManager/src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, RouterModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'courseManager';
+  private router: Router = inject(Router);
+  isAdminView: boolean = false;
+
+  toggleAdminView() {
+    this.isAdminView = !this.isAdminView;
+    if (this.isAdminView) {
+     this.router.navigate(['/admin']);
+    } else {
+      this.router.navigate(['/cursos']);
+    }
+  }
 }
